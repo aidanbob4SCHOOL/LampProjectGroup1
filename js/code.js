fb846fdc0c1c07dee95d60125aab6f196d6bbdae
Dylan Gonzalez <dyfgonz@gmail.com>
2026-02-13 02:17:27 -0500

    added save and read cookie (oopsie) also put a bunch of console logs in doLogin() just in case

--- js/code.js
+++ js/code.js
@@ -196,6 +196,47 @@ let firstName = "";
 let lastName = "";
 const ids = [];
 
+function saveCookie() {
+    let minutes = 20;
+    let date = new Date();
+    date.setTime(date.getTime() + (minutes * 60 * 1000));
+
+    document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie() {
+    userId = -1;
+    let data = document.cookie;
+    let splits = data.split(",");
+
+    for (var i = 0; i < splits.length; i++) {
+
+        let thisOne = splits[i].trim();
+        let tokens = thisOne.split("=");
+
+        if (tokens[0] == "firstName") {
+            firstName = tokens[1];
+        }
+
+        else if (tokens[0] == "lastName") {
+            lastName = tokens[1];
+        }
+
+        else if (tokens[0] == "userId") {
+            userId = parseInt(tokens[1].trim());
+        }
+    }
+
+    if (userId < 0) {
+        window.location.href = "index.html";
+    }
+
+    else {
+        // Welcome User
+        // document.getElementById("userName").innerHTML = "Welcome, " + firstName + " " + lastName + "!";
+    }
+}
+
 function doLogin() {
     userId = 0;
     firstName = "";
@@ -224,17 +265,22 @@ function doLogin() {
 
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
+    console.log("Opened a POST to: " + url);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
     try {
         xhr.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-
+                
+                console.log("Response:" + xhr.responseText);
                 let jsonObject = JSON.parse(xhr.responseText);
+                console.log("JSON parsed");
                 userId = jsonObject.id;
+                console.log(userId);
 
                 if (userId < 1) {
                     document.getElementById("result").style.display = "block";
                     document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+                    console.log("userID invalid: Invalid login?" );
                     return;
                 }
                 firstName = jsonObject.firstName;
@@ -242,13 +288,16 @@ function doLogin() {
 
                 saveCookie();
                 window.location.href = "contacts.html";
+                console.log("href Changed to " + window.location.href);
             }
         };
 
         xhr.send(jsonPayload);
+        console.log("Payload Sent!");
     } catch (err) {
         document.getElementById("result").style.display = "block";
         document.getElementById("loginResult").innerHTML = err.message;
+        console.log("Something went wrong with JSON: " + err.message);
     }
 }
 
